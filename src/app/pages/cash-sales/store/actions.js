76e1a7d6a46afbb4895774621aa6a23f0f5fc2d4
Nana Adjei <nana.elvee@gmail.com>
2021-01-07 10:38:52 +0000

    Remove unnecessary try/catch

--- src/app/pages/cash-sales/store/actions.js
+++ src/app/pages/cash-sales/store/actions.js
@@ -21,27 +21,18 @@ export const updateSalesItems = ({ getters, commit }) => {
  * @param { object } saleObj - The saleitems data to be stored in the database. Ie; specified_data and items
  */
 export const saveNewSalesItems = ({ commit, dispatch }, saleObj) => {
-    try {
-        HttpRequest({ url: "sales", type: "post", user: "admin", data: saleObj }).then(() => {
-            /** Set sales items back to empty to clear items in table */
-            commit("RESET_SALE_ITEM", []);
-            window.vm.$toast.success("Products successfully saved");
-            /** Make a call to get fresh products so we can see the deducted products been updated */
-            dispatch("fetchProductsAsync");
-        });
-
-    } catch (error) {
-        throw error;
-    }
+    HttpRequest({ url: "sales", type: "post", user: "admin", data: saleObj }).then(() => {
+        /** Set sales items back to empty to clear items in table */
+        commit("RESET_SALE_ITEM", []);
+        window.vm.$toast.success("Products successfully saved");
+        /** Make a call to get fresh products so we can see the deducted products been updated */
+        dispatch("fetchProductsAsync");
+    });
 };
 
 /** Print items with POS printer */
 export const printSaleItems = (saleObj) => {
-    try {
-        HttpRequest({ url: "sales/print/invoice", type: "post", user: "admin", data: saleObj }).then(() => {
-            // console.log("Items printing...");
-        });
-    } catch (error) {
-        throw error;
-    }
+    HttpRequest({ url: "sales/print/invoice", type: "post", user: "admin", data: saleObj }).then(() => {
+        // console.log("Items printing...");
+    });
 };
